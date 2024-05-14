7ffdaee17419b1079e8695d630a28d306db0ffcb
daniel ortiz millan <danielortizmillan@MacBook-Air-de-daniel.local>
2024-05-13 18:50:16 -0600

    first commit

---
+++ AlboAirPort/AlboAirPortApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlboAirPortApp.swift
+//  AlboAirPort
+//
+//  Created by daniel ortiz millan on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AlboAirPortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
